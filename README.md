54f3db79b917b90c99eeabb949477002c81185fd
JiaLi Ma <yydw.mjl@foxmail.com>
2022-04-03 15:04:11 +0800

    modified annotation

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ### RecodeIDEM
 
-data_deal：完成数据的前期处理
+data_deal：Complete the pre-processing of data
 
-models：提供模型
+models：Provide model
 
-utils：提供一些实验结果保存功能
+utils：Provide some functions of saving experimental results
 
-main.py：启动，完成训练，保存结果
+main.py：Start, finish training, save results
--- main.py
+++ main.py
@@ -8,43 +8,43 @@ sys.path.append("..")
 sys.path = origin_path
 
 
-parser = argparse.ArgumentParser(description="输入网络运行所需的参数")
+parser = argparse.ArgumentParser(description="Enter the parameters required for network operation")
 
 parser.add_argument("--filePath",      type=str,      default="", required =True,
-                    help="资源文件的地址")
+                    help="Address of the resource file")
 parser.add_argument("--conditionPath", type=str,      default="", required =True,
-                    help="表地址，对应每个数据特征条件注明 0：删除该特征数据 1：连续数据  2：离散数据")
+                    help="Table address, corresponding to each data characteristic condition 0: delete the feature data 1: Continuous data  2: Discrete data")
 
 parser.add_argument("--model_epochs",  type=int,      default=1,
-                    help="模型运行次数 default = 1")
+                    help="Number of model runs default = 1")
 parser.add_argument("--sim_size",      type=int,      default=15000,
-                    help="孪生网络训练数据大小 default = 15000")
+                    help="Twin network training data size default = 15000")
 parser.add_argument("--train_prob",    type=float,    default=0.002,
-                    help="保留使用的训练数据占比, default = 0.002")
+                    help="Proportion of reserved training data, default = 0.002")
 parser.add_argument("--test_prob",     type=float,    default=0.003,
-                    help="保留使用的测试数据占比, default = 0.003")
+                    help="Proportion of retained test data, default = 0.003")
 parser.add_argument("--test_len",      type=float,    default=0.25,
-                    help="原始数据划分，测试数据所占比率, default = 0.25")
+                    help="Division of original data and proportion of test data, default = 0.25")
 parser.add_argument("--epochs",        type=int,      default=300,
-                    help="训练次数, default=300")
+                    help="Training times, default=300")
 parser.add_argument("--batch_size",    type=int,      default=512,
-                    help="批处理大小, default=512")
+                    help="Batch size, default=512")
 parser.add_argument("--pdf",           type=bool,     default=True,
-                    help="是否保存运行图片数据 default=True")
+                    help="Save running picture data default=True")
 parser.add_argument("--show",          type=bool,     default=True,
-                    help="是否显示运行图片数据 default=True")
+                    help="Whether to display running picture data default=True")
 
 parser.add_argument("--epoc",          type=int,     default=1,
-                    help="目前用于生成数据的后缀标志 default=1")
+                    help="Suffix flag currently used to generate data default=1")
 
 parser.add_argument("--CSDNN",         type=bool,     default=False,
-                    help="是否运行CSDNN模型 default=False")
+                    help="Whether to run the CSDNN model default=False")
 parser.add_argument("--SNN",           type=bool,     default=False,
-                    help="是否运行SNN模型 default=False")
+                    help="Whether to run the SNN model default=False")
 parser.add_argument("--FSSNN",         type=bool,     default=False,
-                    help="是否运行FSSNN模型 default=False")
+                    help="Whether to run FSSNN model default=False")
 parser.add_argument("--FSESNN",        type=bool,     default=True,
-                    help="是否运行FSESNN模型 default=True")
+                    help="Whether to run the FSESNN model default=True")
 args = parser.parse_args()
 # print(args)
 
